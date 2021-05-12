197b21ffde4ca54bcfe5d720490330569901f95b
jacob-danner <78774495+jacob-danner@users.noreply.github.com>
2021-05-12 13:55:48 -0500

    version 3
    finished

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MusicVisualizer
 Final project for cs 147 computer graphics
+
+1-Estimate how many hours you spent on the final project
+I would say about 12 hours. I spent about 4 hours learning the ins and outs of audio, 3 hours with three.js, and about 5 putting everything together.
+
+
+2-Briefly (2-3 sentences) describe what you learned through completing your final project
+I learned a lot! I had to really push my javascript skills. I learned how to take in files, use audio, three.js, and a bunch of other random things along the journey.
+
+3-Outof a scale from 0 –100,give a proposed grade for your project. While I will be the final evaluator of your efforts, I would like you to self-assess your effort here. Add a few sentences that explains your score
+100. I ran out of time to perfect it. If I would've had more time I would have gotten experimented much more within three.js and made it look way better/more interesting. But there were a bunch of challenging moving pieces to figure out, and I spent more than the required amount of time on my project.
--- visualizer.js
+++ visualizer.js
@@ -1,9 +1,5 @@
 // set up container div
 const container = document.getElementById('container');
-const canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
 
 // get file
 const file = document.getElementById('fileupload');
@@ -29,28 +25,91 @@ container.addEventListener('click', function(){
 
     //draw 
 
-    const barWidth = canvas.width / bufferLength;
-    let barHeight;
-    let x;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    
+    // create shapes
+    let geometry1 = new THREE.BoxGeometry(1, 1, 1);
+    let material1 = new THREE.MeshBasicMaterial( { color: 'rgb(255, 0,0)' } );
+
+    let geometry2 = new THREE.BoxGeometry(1, 1, 1);
+    let material2 = new THREE.MeshBasicMaterial( { color: 'rgb(255, 0,0)' } );
+
+    let geometry3 = new THREE.BoxGeometry(1, 1, 1);
+    let material3 = new THREE.MeshBasicMaterial( { color: 'rgb(255, 0,0)' } );
+
+    const cube1 = new THREE.Mesh( geometry1, material1 );
+    scene.add( cube1 ); 
+
+    const cube2 = new THREE.Mesh( geometry2, material2 );
+    scene.add( cube2 ); 
+
+    const cube3 = new THREE.Mesh( geometry3, material3 );
+    scene.add( cube3 ); 
+
+    // ring
+    const geometry4 = new THREE.RingGeometry( 1, 5, 16 );
+    const material4 = new THREE.MeshBasicMaterial( { color: 'rgb(255, 0,0)', side: THREE.DoubleSide } );
+    const ring = new THREE.Mesh( geometry4, material4 );
+    scene.add( ring );
+
+    camera.position.z = 5;
+
 
     function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // get audio data at time from dataArray
-        analyser.getByteFrequencyData(dataArray);
-        
-        x = 0;
-        // code to draw 
-        for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
-            ctx.fillStyle = 'white';
-            ctx.fillRect(x, ( canvas.height - barHeight ), barWidth * 2, barHeight);
-            x += barWidth;
-        }
-        
-        // frequency data is whole integers from 0 to 255
+        analyser.getByteFrequencyData(dataArray);  
 
+        // create equally lengthed subsets of dataArray
+        let section = dataArray.length / 4
+        let section1 = dataArray.slice(0, section);
+        let section2 = dataArray.slice(section, section*2);
+        let section3 = dataArray.slice(section*2, section*3);
+
+        // create array of sections
+        const sections = [section1, section2, section3];
+        
+        // compute average of array
+        let avgFrq = ( dataArray.reduce((a, b) => a + b) / dataArray.length ) /100;
+        let s1AvgFrq = ( section1.reduce((a, b) => a + b) / section1.length );
+        let s2AvgFrq = ( section2.reduce((a, b) => a + b) / section2.length );
+        let s3AvgFrq = ( section3.reduce((a, b) => a + b) / section3.length );
+
+        let c1Color = s1AvgFrq/1;
+        let c2Color = s2AvgFrq*2;
+        let c3Color = s3AvgFrq * 4;
+
+
+        cube1.scale.x = innerWidth;
+        cube1.scale.y = s1AvgFrq / 30;
+        cube1.position.set(0, 0, 0);
+        cube1.material.color.set( (255, c1Color, c1Color) );
+        
+
+        cube2.scale.x = innerWidth;
+        cube2.scale.y = s2AvgFrq / 40;
+        cube2.position.set(0, 0, 1);
+        cube2.material.color.set( (255, 255, c2Color) );
+
+        cube3.scale.x = innerWidth;
+        cube3.scale.y = s2AvgFrq / 90;
+        cube3.position.set(0, 0, 2);
+        cube3.material.color.set( (255, 200, c3Color) );
+
+        // ring.scale.x = innerWidth;
+        ring.scale.x = avgFrq * 1.7;
+        ring.scale.y = avgFrq * 1.7;
+        ring.position.set(0, 0, 3);
+        ring.material.color.set( 'red' );
+
+        // render scene
+        renderer.render( scene, camera );
         requestAnimationFrame(animate);
     }
+    
     animate();
 });
 
@@ -61,4 +120,3 @@ file.addEventListener('change', function() {
     audio.load();
     audio.play();
 })
-
